acefe330ca24676c81ee21fd4a77f623f0230634
Micky5991 <micky5991@gmail.com>
2018-01-12 14:39:08 +0100

    Add .NET Standard 2.0 AlternateVoice.Server Project

---
+++ AlternateVoice.Server/src/Interfaces/IVoiceClient.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using AlternateVoice.Server.Structs;
+
+namespace AlternateVoice.Server.Interfaces
+{
+    public interface IVoiceClient
+    {
+        
+        VoiceHandle Handle { get; }
+        
+        bool Microphone { get; set; }
+        bool Headphones { get; set; }
+        
+        IEnumerable<IVoiceGroup> Groups { get; }
+
+        void JoinGroup(IVoiceGroup group);
+        void LeaveGroup(IVoiceGroup group);
+
+    }
+}
