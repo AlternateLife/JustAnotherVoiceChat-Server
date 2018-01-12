da6638e7d9aadc80b8e21ddeded8e1726313de63
Micky5991 <micky5991@gmail.com>
2018-01-12 14:39:08 +0100

    Add .NET Standard 2.0 AlternateVoice.Server Project

---
+++ AlternateVoice.Server/src/Elements/VoiceServer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlternateVoice.Server.Interfaces;
+using AlternateVoice.Server.Structs;
+
+namespace AlternateVoice.Server.Elements
+{
+    public class VoiceServer : IVoiceServer
+    {
+        private readonly string _hostname;
+        private readonly ushort _port;
+        private readonly int _channelId;
+        
+        private readonly List<VoiceGroup> _groups = new List<VoiceGroup>();
+
+        internal VoiceServer(string hostname, ushort port, int channelId)
+        {
+            _hostname = hostname;
+            _port = port;
+            _channelId = channelId;
+        }
+        
+        public void Start()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string CreateConnectionString()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IVoiceGroup CreateGroup()
+        {
+            var createdGroup = new VoiceGroup();
+            lock (_groups)
+            {
+                _groups.Add(createdGroup);
+            }
+
+            return createdGroup;
+        }
+
+        public IEnumerable<IVoiceGroup> GetAllGroups()
+        {
+            lock (_groups)
+            {
+                return _groups.ToList();
+            }
+        }
+
+        public void DestroyGroup(IVoiceGroup voiceGroup)
+        {
+            if (voiceGroup == null)
+            {
+                throw new ArgumentNullException(nameof(voiceGroup));
+            }
+            
+            voiceGroup.Dispose();
+        }
+
+        public IVoiceClient GetClientByHandle(VoiceHandle handle)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
