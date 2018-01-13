2390b9172c62746d8790a5cb1b3700285b80c726
Micky5991 <micky5991@gmail.com>
2018-01-13 15:27:28 +0100

    Start Dummy CS Server

---
+++ AlternateLife.Server.Dummy/src/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AlternateLife.Server.Dummy
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO, LUL");
+        }
+        
+    }
+}
