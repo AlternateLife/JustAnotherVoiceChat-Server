d99dbc500076b9c13782921d3f468124ba7acb36
Micky5991 <micky5991@gmail.com>
2018-01-13 01:33:38 +0100

    Start first implementations of the .GTMP adapter

---
+++ AlternateVoice.Server.GTMP/src/Exceptions/GtmpElementCreationException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AlternateVoice.Server.GTMP.Exceptions
+{
+    public class GtmpElementCreationException : Exception
+    {
+
+        public GtmpElementCreationException(Type type) : base($"Failed to store \"{type}\" into FactoryCollection")
+        {
+            
+        }
+        
+    }
+}
