8fd6b0937b828cd0260294a30b018d479e1fa1ae
Micky5991 <micky5991@gmail.com>
2018-01-28 23:23:27 +0100

    VoiceServerTest now invokes the new handlers

--- AlternateVoice.Server.Wrapper/src/Elements/Server/VoiceServerTest.cs
+++ AlternateVoice.Server.Wrapper/src/Elements/Server/VoiceServerTest.cs
@@ -1,26 +1,15 @@
-﻿using AlternateVoice.Server.Wrapper.Elements.Client;
-using AlternateVoice.Server.Wrapper.Enums;
-
-namespace AlternateVoice.Server.Wrapper.Elements.Server
+﻿namespace AlternateVoice.Server.Wrapper.Elements.Server
 {
     internal partial class VoiceServer
     {
         public void TriggerClientConnectedEvent(ushort handle)
         {
-            VoiceClient client;
-            if (_clients.TryGetValue(handle, out client))
-            {
-                OnClientConnecting?.Invoke(client);
-            }
+            OnClientConnectedFromVoice(handle);
         }
 
         public void TriggerClientDisconnectedEvent(ushort handle)
         {
-            VoiceClient client;
-            if (_clients.TryGetValue(handle, out client))
-            {
-                OnClientDisconnected?.Invoke(client, DisconnectReason.None);
-            }
+            OnClientDisconnectedFromVoice(handle);
         }
     }
 }
