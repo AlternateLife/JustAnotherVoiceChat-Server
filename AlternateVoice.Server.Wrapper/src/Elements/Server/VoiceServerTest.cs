1890949958b1319a996cd850f28649d7b05cabee
Micky5991 <micky5991@gmail.com>
2018-01-28 23:14:21 +0100

    Add VoiceServer Testing partial to add manual event triggers

---
+++ AlternateVoice.Server.Wrapper/src/Elements/Server/VoiceServerTest.cs
@@ -0,0 +1,26 @@
+﻿using AlternateVoice.Server.Wrapper.Elements.Client;
+using AlternateVoice.Server.Wrapper.Enums;
+
+namespace AlternateVoice.Server.Wrapper.Elements.Server
+{
+    internal partial class VoiceServer
+    {
+        public void TriggerClientConnectedEvent(ushort handle)
+        {
+            VoiceClient client;
+            if (_clients.TryGetValue(handle, out client))
+            {
+                OnClientConnecting?.Invoke(client);
+            }
+        }
+
+        public void TriggerClientDisconnectedEvent(ushort handle)
+        {
+            VoiceClient client;
+            if (_clients.TryGetValue(handle, out client))
+            {
+                OnClientDisconnected?.Invoke(client, DisconnectReason.None);
+            }
+        }
+    }
+}
