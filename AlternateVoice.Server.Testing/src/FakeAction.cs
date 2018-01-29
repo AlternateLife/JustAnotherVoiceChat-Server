0e7a099c041ba30023c41535ed3acadbc3b27772
Micky5991 <micky5991@gmail.com>
2018-01-29 23:27:47 +0100

    Add FakeAction-Class that counts action invocations

---
+++ AlternateVoice.Server.Testing/src/FakeAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AlternateVoice.Server.Testing
+{
+    public class FakeAction<T>
+    {
+        
+        public int InvokeAmount { get; private set; }
+        public Action<T> Action { get; }
+
+        public FakeAction()
+        {
+            Action = e => InvokeAmount++;
+        }
+        
+    }
+}
