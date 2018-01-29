8a6954257a461ece8c0c8744a089862ab9486d54
Micky5991 <micky5991@gmail.com>
2018-01-29 22:54:45 +0100

    Start first unit-testing project

---
+++ AlternateVoice.Server.Wrapper.Tests/src/AlternateVoiceFixture.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace AlternateVoice.Server.Wrapper.Tests
+{
+    [TestFixture]
+    public class AlternateVoiceFixture
+    {
+        [Test]
+        public void MakingServerWontStoreResultItInStaticProperty()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+            
+        }
+    }
+}
