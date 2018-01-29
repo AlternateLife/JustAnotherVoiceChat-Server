76e4f695eba30658fd1779d9f7d5f7852b5a91e5
Micky5991 <micky5991@gmail.com>
2018-01-29 13:09:56 +0100

    Move voicescript events and eventhandlers to seperate partial

--- AlternateVoice.Server.GTMP.Resource/Server/VoiceScript.cs
+++ AlternateVoice.Server.GTMP.Resource/Server/VoiceScript.cs
@@ -1,9 +1,7 @@
 ﻿using AlternateVoice.Server.GTMP.Factories;
 using AlternateVoice.Server.GTMP.Interfaces;
 using GrandTheftMultiplayer.Server.API;
-using GrandTheftMultiplayer.Server.Constant;
 using GrandTheftMultiplayer.Server.Elements;
-using GrandTheftMultiplayer.Shared.Gta.Tasks;
 
 namespace AlternateVoice.Server.GTMP.Resource
 {
@@ -16,39 +14,12 @@ namespace AlternateVoice.Server.GTMP.Resource
         {
             _voiceServer = GtmpVoice.CreateServer(API, "game.alternate-life.de", 23332, 23);
 
-            _voiceServer.OnServerStarted += () =>
-            {
-                API.consoleOutput(LogCat.Info, $"GtmpVoiceServer started, listening on {_voiceServer.Hostname}:{_voiceServer.Port}");
-            };
-            
-            _voiceServer.OnServerStopping += () =>
-            {
-                API.consoleOutput(LogCat.Info, "GtmpVoiceServer stopping!");
-            };
-
-            _voiceServer.OnClientPrepared += c =>
-            {
-                c.Player.triggerEvent("VOICE_SET_HANDSHAKE", true, c.HandshakeUrl);
-            };
-            
-            _voiceServer.OnClientConnected += c =>
-            {
-                c.Player.triggerEvent("VOICE_SET_HANDSHAKE", false);
-            };
-            
-            _voiceServer.OnClientDisconnected += c =>
-            {
-                c.Player.triggerEvent("VOICE_SET_HANDSHAKE", true, c.HandshakeUrl);
-            };
-
-            _voiceServer.OnPlayerStartsTalking += OnPlayerStartsTalking;
-            _voiceServer.OnPlayerStopsTalking += OnPlayerStopsTalking;
+            AttachServerEvents(_voiceServer);
 
             API.onClientEventTrigger += OnClientEventTrigger;
+            API.onResourceStop += OnResourceStop;
 
             _voiceServer.Start();
-            
-            API.onResourceStop += OnResourceStop;
         }
 
         private void OnClientEventTrigger(Client sender, string eventName, params object[] arguments)
@@ -67,23 +38,6 @@ namespace AlternateVoice.Server.GTMP.Resource
             }
 
             voiceClient.CameraRotation = rotation;
-        }
-
-        private void OnPlayerStartsTalking(IGtmpVoiceClient speakingClient)
-        {
-            API.playPlayerAnimation(speakingClient.Player, (int)(AnimationFlag.Loop | AnimationFlag.AllowRotation), "mp_facial", "mic_chatter");
-            // Needs further investigation of animation handling 
-            //API.sendNativeToPlayersInRange(speakingClient.Player.position, 300f, Hash.TASK_PLAY_ANIM, speakingClient.Player.handle, "mp_facial", "mic_chatter",
-            //  8f, -4f, -1, (int)(AnimationFlag.Loop | AnimationFlag.AllowRotation), 0.0f, false, false, false);
-        }
-
-        private void OnPlayerStopsTalking(IGtmpVoiceClient speakingClient)
-        {
-            // Stopping all animations?
-            API.stopPlayerAnimation(speakingClient.Player);
-            // Needs further investigation of animation handling 
-            // API.sendNativeToPlayersInRange(speakingClient.Player.position, 300f, Hash.TASK_PLAY_ANIM, speakingClient.Player.handle, "mp_facial", "mic_chatter1",
-            //    8f, -4f, -1, (int)(AnimationFlag.Loop | AnimationFlag.AllowRotation), 0.0f, false, false, false);
         }
 
         private void OnResourceStop()
