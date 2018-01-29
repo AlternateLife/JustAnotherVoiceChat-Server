994a51f5f6af7ef2ec3f5ce277365500fac73569
Markus Grigull <web@grigull.me>
2018-01-29 20:27:45 +0100

    Add server creation test

--- AlternateVoice.Server/test/test.cpp
+++ AlternateVoice.Server/test/test.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 
+#include "AlternateVoice.h"
+
 int main() {
-  return 0;
+  // create server
+  auto server = new AlternateVoice::AlternateVoiceServer(1234);
+  if (server->create() == false) {
+    std::cerr << "Unable to create AlternateVoice server on port 1234" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::cout << "AlternateVoice server created on port 1234" << std::endl;
+
+  // clean up
+  server->close();
+  delete server;
+
+  return EXIT_SUCCESS;
 }
