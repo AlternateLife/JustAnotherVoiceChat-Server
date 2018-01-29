1ddc3b32402f61b931130643074473d7f748694a
Micky5991 <micky5991@gmail.com>
2018-01-29 23:25:01 +0100

    Start VoiceServer UnitTests

---
+++ AlternateVoice.Server.Wrapper.Tests/src/VoiceServerFixtures.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using AlternateVoice.Server.Wrapper.Elements.Server;
+using AlternateVoice.Server.Wrapper.Exceptions;
+using AlternateVoice.Server.Wrapper.Interfaces;
+using Moq;
+using NUnit.Framework;
+
+namespace AlternateVoice.Server.Wrapper.Tests
+{
+    [TestFixture]
+    public class VoiceServerFixtures
+    {
+
+        [Test]
+        public void ConstructorWillThrowExceptionIfRepositoryIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var server = new VoiceServer(null, "localhost", 23332, 23);
+            });
+        }
+
+        [Test]
+        public void ConstructorWithInvalidHostnameWillThrowInvalidHostnameException()
+        {
+            var repository = new Mock<IVoiceClientRepository>();
+            
+            var invalidhostnames = new [] { "ä#äää", "" };
+
+            foreach (var invalidhostname in invalidhostnames)
+            {
+                Assert.Throws<InvalidHostnameException>(() =>
+                {
+                    var server = new VoiceServer(repository.Object, invalidhostname, 23332, 23);
+                });
+            }
+        }
+
+        [Test]
+        public void StartingVoiceServerWillSetStartedPropertyToTrue()
+        {
+            var repository = new Mock<IVoiceClientRepository>();
+            var server = new VoiceServer(repository.Object, "localhost", 23332, 23);
+            
+            server.Start();
+            
+            
+            
+        }
+        
+    }
+}
