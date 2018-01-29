2230a57e56c244208c9187abe9fed88a0c2adbf7
Micky5991 <micky5991@gmail.com>
2018-01-29 12:58:03 +0100

    Seperate commands to dedicated file

---
+++ AlternateVoice.Server.GTMP.Resource/Server/VoiceScript.Commands.cs
@@ -0,0 +1,36 @@
+﻿using GrandTheftMultiplayer.Server.Elements;
+using GrandTheftMultiplayer.Server.Managers;
+
+namespace AlternateVoice.Server.GTMP.Resource
+{
+    public partial class VoiceScript
+    {
+        
+        [Command("lip", "Usage: /lip [true/false]")]
+        public void SetLipSync(Client sender, bool active)
+        {
+            if (active)
+            {
+                _voiceServer.TestLipSyncActiveForPlayer(sender);
+            }
+            else
+            {
+                _voiceServer.TestLipSyncInactiveForPlayer(sender);
+            }
+        }
+        
+        [Command("connect")]
+        public void VoicePlayerConnect(Client sender, ushort handle, bool connected)
+        {
+            if (connected)
+            {
+                _voiceServer.TriggerOnClientConnectedEvent(handle);
+            }
+            else
+            {
+                _voiceServer.TriggerOnClientDisonnectedEvent(handle);
+            }
+        }
+        
+    }
+}
