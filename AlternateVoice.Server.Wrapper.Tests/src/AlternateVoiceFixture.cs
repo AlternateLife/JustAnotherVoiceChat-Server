66baa1dabc806a8a31b89756a6cba8242200dbf4
Micky5991 <micky5991@gmail.com>
2018-01-29 23:08:23 +0100

    Add unit-tests to check for exceptions

--- AlternateVoice.Server.Wrapper.Tests/src/AlternateVoiceFixture.cs
+++ AlternateVoice.Server.Wrapper.Tests/src/AlternateVoiceFixture.cs
@@ -1,4 +1,6 @@
-﻿using AlternateVoice.Server.Wrapper.Interfaces;
+﻿using System;
+using AlternateVoice.Server.Wrapper.Interfaces;
+using Moq;
 using NUnit.Framework;
 
 namespace AlternateVoice.Server.Wrapper.Tests
@@ -9,10 +11,18 @@ namespace AlternateVoice.Server.Wrapper.Tests
         [Test]
         public void MakingServerWillReturnNewVoiceServerInstance()
         {
-            var result = AlternateVoice.MakeServer(null, "localhost", 23332, 20);
+            var repositoryMock = new Mock<IVoiceClientRepository>();
+            
+            var result = AlternateVoice.MakeServer(repositoryMock.Object, "localhost", 23332, 20);
 
             Assert.NotNull(result);
             Assert.IsInstanceOf<IVoiceServer>(result);
+        }
+        
+        [Test]
+        public void GivingNullAsRepositoryWillThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => { AlternateVoice.MakeServer(null, "localhost", 23332, 20); });
         }
     }
 }
