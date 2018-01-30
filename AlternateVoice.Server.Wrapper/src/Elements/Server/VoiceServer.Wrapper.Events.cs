908574cb2639384ea32869b3949213f2255dbac3
Micky5991 <micky5991@gmail.com>
2018-01-30 13:03:30 +0100

    Reorganize VoiceServer-class

---
+++ AlternateVoice.Server.Wrapper/src/Elements/Server/VoiceServer.Wrapper.Events.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace AlternateVoice.Server.Wrapper.Elements.Server
+{
+    internal partial class VoiceServer
+    {
+        
+        private readonly List<GCHandle> _garbageCollectorHandles = new List<GCHandle>();
+
+        private void RegisterEvent<T>(Action<T> register, T callback)
+        {
+            _garbageCollectorHandles.Add(GCHandle.Alloc(callback));
+            
+            register(callback);
+        }
+        
+        private void AttachToNativeEvents()
+        {
+            RegisterEvent<ClientCallback>(AV_RegisterNewClientCallback, OnClientConnectedFromVoice);
+        }
+
+        private void DisposeNativeEvents()
+        {
+            AV_UnregisterNewClientCallback();
+
+            foreach (var handle in _garbageCollectorHandles)
+            {
+                handle.Free();
+            }
+        }
+        
+    }
+}
---
+++ AlternateVoice.Server.Wrapper/src/Elements/Server/VoiceServer.Wrapper.Testing.cs
@@ -0,0 +1,29 @@
+﻿using AlternateVoice.Server.Wrapper.Interfaces;
+
+namespace AlternateVoice.Server.Wrapper.Elements.Server
+{
+    internal partial class VoiceServer
+    {
+        
+        public void FireClientStartsSpeaking(IVoiceClient client)
+        {
+            OnClientStartsTalking?.Invoke(client);
+        }
+
+        public void FireClientStopsSpeaking(IVoiceClient client)
+        {
+            OnClientStopsTalking?.Invoke(client);
+        }
+        
+        public void FireClientConnected(ushort handle)
+        {
+            AVTest_CallNewClientCallback(handle);
+        }
+
+        public void FireClientDisconnected(ushort handle)
+        {
+            OnClientDisconnectedFromVoice(handle);
+        }
+        
+    }
+}
