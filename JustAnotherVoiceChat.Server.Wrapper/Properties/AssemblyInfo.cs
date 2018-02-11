ed3c25759e021ec7c31f4642cd81d7b9846ba299
Micky5991 <micky5991@gmail.com>
2018-02-11 23:19:53 +0100

    Fix project references in wrapper tests and dummy

--- JustAnotherVoiceChat.Server.Wrapper/Properties/AssemblyInfo.cs
+++ JustAnotherVoiceChat.Server.Wrapper/Properties/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("AlternateVoice.Server.Wrapper.Tests")]
+[assembly: InternalsVisibleTo("JustAnotherVoiceChat.Server.Wrapper.Tests")]
