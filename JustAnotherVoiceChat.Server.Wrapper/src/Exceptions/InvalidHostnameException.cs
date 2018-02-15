552fb1046128c8d88c7adb7c520e326f899512ff
dr1zzle <luke12sky@web.de>
2018-02-15 21:11:38 +0100

    Change license headers of all files

--- JustAnotherVoiceChat.Server.Wrapper/src/Exceptions/InvalidHostnameException.cs
+++ JustAnotherVoiceChat.Server.Wrapper/src/Exceptions/InvalidHostnameException.cs
@@ -1,10 +1,10 @@
 ﻿/*
  * File: InvalidHostnameException.cs
- * Date: 11.2.2018,
+ * Date: 15.2.2018,
  *
  * MIT License
  *
- * Copyright (c) 2018 Alternate-Life.de
+ * Copyright (c) 2018 JustAnotherVoiceChat
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
