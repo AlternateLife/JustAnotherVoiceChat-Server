d53575fd034cb59da938a910db7e8cce4cd3d5b6
Micky5991 <micky5991@gmail.com>
2018-02-20 16:41:27 +0100

    Add API to PositionalVoiceTask and change consoleOutput

--- JustAnotherVoiceChat.Server.GTMP.Resource/Server/Tasks/PositionalVoiceTask.cs
+++ JustAnotherVoiceChat.Server.GTMP.Resource/Server/Tasks/PositionalVoiceTask.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using GrandTheftMultiplayer.Server.API;
+using GrandTheftMultiplayer.Server.Constant;
 using JustAnotherVoiceChat.Server.Wrapper.Interfaces;
 
 namespace JustAnotherVoiceChat.Server.GTMP.Resource.Tasks
 {
     public class PositionalVoiceTask<TClient> : IVoiceTask<TClient> where TClient : IVoiceClient<TClient>
     {
+        private readonly API _api;
+
+        public PositionalVoiceTask(API api)
+        {
+            _api = api;
+        }
 
         public async Task RunVoiceTask(IVoiceServer<TClient> server)
         {
@@ -16,7 +24,7 @@ namespace JustAnotherVoiceChat.Server.GTMP.Resource.Tasks
             {
                 native.SetListenerPosition(client, client.Position, client.CameraRotation);
                 
-                Console.WriteLine("SET POSITION: " + client.Handle.Identifer + " -> " + client.Position + " -> " + client.CameraRotation);
+                _api.consoleOutput(LogCat.Debug, "SET POSITION: " + client.Handle.Identifer + " -> " + client.Position + " -> " + client.CameraRotation);
             }
             
             await Task.Delay(50).ConfigureAwait(false);
