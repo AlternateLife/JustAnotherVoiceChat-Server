c27456570fd18db9e00417ce2c62a70ca04208c7
Micky5991 <micky5991@gmail.com>
2018-02-21 04:03:31 +0100

    Hide constructors and types that aren't intended for exposure

--- JustAnotherVoiceChat.Server.GTMP/src/Exceptions/ServerNotSharedException.cs
+++ JustAnotherVoiceChat.Server.GTMP/src/Exceptions/ServerNotSharedException.cs
@@ -5,7 +5,7 @@ namespace JustAnotherVoiceChat.Server.GTMP.Exceptions
     public class ServerNotSharedException : Exception
     {
 
-        public ServerNotSharedException() : base("This feature is only available if the shared GtmpVoice-Server is set")
+        internal ServerNotSharedException() : base("This feature is only available if the shared GtmpVoice-Server is set")
         {
             
         }
