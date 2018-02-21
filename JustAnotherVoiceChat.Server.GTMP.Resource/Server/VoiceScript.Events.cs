f1f1fbf6e8ecd74c3327d935c402c533cea4f5b0
Micky5991 <micky5991@gmail.com>
2018-02-21 04:13:24 +0100

    Remove development comments in VoiceScript lipsync

--- JustAnotherVoiceChat.Server.GTMP.Resource/Server/VoiceScript.Events.cs
+++ JustAnotherVoiceChat.Server.GTMP.Resource/Server/VoiceScript.Events.cs
@@ -78,19 +78,11 @@ namespace JustAnotherVoiceChat.Server.GTMP.Resource
             if (newStatus)
             {
                 API.playPlayerAnimation(speakingClient.Player, (int)(AnimationFlag.Loop | AnimationFlag.AllowRotation), "mp_facial", "mic_chatter");
-                // Needs further investigation of animation handling 
-                //API.sendNativeToPlayersInRange(speakingClient.Player.position, 300f, Hash.TASK_PLAY_ANIM, speakingClient.Player.handle, "mp_facial", "mic_chatter",
-                //  8f, -4f, -1, (int)(AnimationFlag.Loop | AnimationFlag.AllowRotation), 0.0f, false, false, false);
             }
             else
             {
-                // Stopping all animations?
                 API.stopPlayerAnimation(speakingClient.Player);
-                // Needs further investigation of animation handling 
-                // API.sendNativeToPlayersInRange(speakingClient.Player.position, 300f, Hash.TASK_PLAY_ANIM, speakingClient.Player.handle, "mp_facial", "mic_chatter1",
-                //    8f, -4f, -1, (int)(AnimationFlag.Loop | AnimationFlag.AllowRotation), 0.0f, false, false, false);
             }
-            
         }
         
     }
