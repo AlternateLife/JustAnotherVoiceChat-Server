05e6a2ce9020822ddde63335f3132cb3cc6655a2
Micky5991 <micky5991@gmail.com>
2018-02-27 02:21:48 +0100

    Start RageMP Bridge VoiceScript

---
+++ JustAnotherVoiceChat.Server.RageMP.Resource/Server/VoiceScript.VoiceEvents.cs
@@ -0,0 +1,10 @@
+﻿namespace JustAnotherVoiceChat.Server.RageMP.Resource.Server
+{
+    public partial class VoiceScript
+    {
+        private void AttachToVoiceServerEvents()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ JustAnotherVoiceChat.Server.RageMP.Resource/Server/VoiceScript.cs
@@ -0,0 +1,62 @@
+﻿using GTANetworkAPI;
+using JustAnotherVoiceChat.Server.RageMP.Extensions;
+using JustAnotherVoiceChat.Server.RageMP.Factories;
+using JustAnotherVoiceChat.Server.RageMP.Interfaces;
+using JustAnotherVoiceChat.Server.Wrapper.Elements.Models;
+using JustAnotherVoiceChat.Server.Wrapper.Elements.Tasks;
+
+namespace JustAnotherVoiceChat.Server.RageMP.Resource.Server
+{
+    public partial class VoiceScript : Script
+    {
+
+        private IRagempVoiceServer _voiceServer;
+        
+        public VoiceScript()
+        {
+            
+        }
+
+        [ServerEvent(Event.ResourceStart)]
+        public void ResourceStartEvent()
+        {
+            // Create a JustAnotherVoiceServer based GtmpVoice Server!
+            var hostname = NAPI.Resource.GetResourceSetting<string>("justanothervoicechat", "voice_host");
+            var port = NAPI.Resource.GetResourceSetting<ushort>("justanothervoicechat", "voice_port");
+            
+            var teamspeakServerId = NAPI.Resource.GetResourceSetting<string>("justanothervoicechat", "voice_teamspeak_serverid");
+            var teamspeakChannelId = NAPI.Resource.GetResourceSetting<ulong>("justanothervoicechat", "voice_teamspeak_channelid");
+            var teamspeakChannelPassword = NAPI.Resource.GetResourceSetting<string>("justanothervoicechat", "voice_teamspeak_channelpassword");
+            
+            _voiceServer = RagempVoice.CreateServer(new VoiceServerConfiguration(hostname, port, teamspeakServerId, teamspeakChannelId, teamspeakChannelPassword));
+            
+            // Enables 3D Voice
+            _voiceServer.AddTask(new PositionalVoiceTask<IRagempVoiceClient>());
+
+            // Attach to some IVoiceServer events to react to certain them with certain actions.
+            AttachToVoiceServerEvents();
+
+            // Startup VoiceServer, so players are able to connect.
+            _voiceServer.Start();
+        }
+
+        [ServerEvent(Event.PlayerConnected)]
+        public void PlayerConnected(Client client)
+        {
+            _voiceServer.BridgeOnClientConnectedEvent(client);
+        }
+
+        [ServerEvent(Event.PlayerDisconnected)]
+        public void PlayerDisconnected(Client client)
+        {
+            _voiceServer.BridgeOnClientDisconnectedEvent(client);
+        }
+
+        [RemoteEvent("VOICE_ROTATION")]
+        public void UpdateVoiceRotation(Client sender, float rotation)
+        {
+            sender.SetVoiceRotation(rotation);
+        }
+        
+    }
+}
