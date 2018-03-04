16824375e2c3899430b51f80cf14318a1687acbb
Markus Grigull <web@grigull.me>
2018-03-04 21:30:29 +0100

    Add trace log message to server update

--- JustAnotherVoiceChat.Server/src/server.cpp
+++ JustAnotherVoiceChat.Server/src/server.cpp
@@ -453,7 +453,9 @@ void Server::update() {
   ENetEvent event;
 
   while (_running) {
+    logMessage("Locking in update", LOG_LEVEL_TRACE);
     std::lock_guard<std::mutex> guard(_serverMutex);
+    logMessage("Locked in update", LOG_LEVEL_TRACE);
     if (_server == nullptr) {
       return;
     }
